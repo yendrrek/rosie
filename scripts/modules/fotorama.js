1a7de544456b16715ec35e06cc2e66ee1c8e386d
root <root@yendrrek-ThinkPad-SL510>
2021-07-17 17:41:31 +0100

    Load Fotorama only when it's needed.
    Load gallery's script and css for devices which are actually using it
    (touchscreens narrower than 1170px).
    Save bandwidth.

---
+++ scripts/modules/fotorama.js
@@ -0,0 +1,33 @@
+'use strict';
+
+export const Fotorama = {
+ 
+  head: document.head,
+  shopPageIsLoaded: (document.querySelector('.breadcrumbs__title').innerText === 'Shop'),
+  fotoramaJsAlreadyInDom: () => document.querySelector('script[src*="fotorama"]'),
+  fotoramaCssAlreadyInDom: () => document.querySelector('link[href*="fotorama"]'),
+
+  insertFotoramaForScreensNarrowerThan1170px () {
+    const fotoramaCss = document.createElement('link');
+    const fotoramaJs = document.createElement('script');
+    fotoramaCss.href = 'fotorama-4.6.4.dev/fotorama.dev.css';
+    fotoramaCss.rel = 'stylesheet';
+    fotoramaJs.src = 'fotorama-4.6.4.dev/fotorama.dev.js';
+    if (!this.fotoramaJsAlreadyInDom() && !this.fotoramaCssAlreadyInDom()) {
+      if (screen.width < 1170 && this.shopPageIsLoaded ) {
+        this.head.append(fotoramaCss);
+        this.head.append(fotoramaJs);
+      }
+    } 
+  },
+
+  removeFotoramaForScreensWiderThan1169px () {
+    if (this.fotoramaJsAlreadyInDom() && this.fotoramaCssAlreadyInDom()) {
+  	  if (screen.width > 1169 && this.shopPageIsLoaded) {
+	    this.fotoramaJsAlreadyInDom().remove();
+	    this.fotoramaCssAlreadyInDom().remove();
+      }
+    }
+  }
+
+};
