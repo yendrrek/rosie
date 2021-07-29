29cd8dd7f1a71bd7e6b0ea586200234d30adf045
Andrzej Piontek <yendrrek@protonmail.com>
2021-07-29 20:43:53 +0100

    Load modules with code affecting Safari only if a Safari browser is used

--- scripts/modules/other-modules.js
+++ scripts/modules/other-modules.js
@@ -5,5 +5,3 @@ export { Navigation } from './navigation.js';
 export { OperationsInsideBasket } from './operations-inside-basket.js';
 
 export { OutlineForKeyboardUsers } from './outline-for-keyboard-users.js';
-
-export { SafariFixStyles } from './safari-fix-styles.js';
