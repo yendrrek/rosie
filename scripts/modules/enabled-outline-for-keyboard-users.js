7a932108567d3de5c0370fbf5fc47adaa3dd1810
Andrzej Piontek <yendrrek@protonmail.com>
2021-07-30 19:54:08 +0100

    Load module showing outline for keyboard users only when it is needed
    A separate module showing outline for contact form fields is created as well.

---
+++ scripts/modules/enabled-outline-for-keyboard-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+export const EnabledOutlineForKeyboardUsers = {
+
+  focusableElements: () => document.querySelectorAll(`
+    .arrow_outline, 
+    .basket_outline,
+    .btn_add-to-basket_outline,
+    .btn-basket_outline,
+    .btn_send-contact-form_outline,
+    .close-popup-btn_outline,
+    .extra-img-activator_outline,
+    .icon_zoom-and-close_outline,
+    .link_breadcrumbs-home_outline,
+    .link_font_outline,
+    .link_instagram_outline,
+    .link_shop-info-font_italic_outline,
+    .logo_outline, 
+    .nav__link_outline,
+    .policy-activator_outline,
+    .subnav__link_outline,
+    .thumbnail-clickable-area_outline,
+    .qty-box_outline
+ `),
+
+  enableOutline () {
+    document.addEventListener('focusin', this.showOutline());
+  },
+
+  showOutline () {
+    for (const elements of this.focusableElements()) {
+      elements.classList.remove('outline-none');
+    }
+  },
+
+};
