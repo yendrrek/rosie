35edbc4b0cca5340b591d0c4df43898253788727
root <root@rosie-ThinkPad-X200s>
2021-08-03 10:54:34 +0100

    Update documentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
-/* All imports are object literals.
-Data needed across different methods are stored in properties.
-Data needed in only one method in a particular object are made variables.
-
-All lightboxes are shown in full-page and consist of two animated modals, one is the background, the other is in the center and contains the content.
-
-'Resize' event is fired when changing viewport width via developer tools.
+/**
+ * All modules are loaded dynamically. It probably is overkill as loading all code even if it is not immediately needed does not cause any performance issues but I wanted to practice dynamic loading. It also helped me simplify some code. 
+ 
+ * All lightboxes are shown in full-page and consist of two animated modals, one is the background, the other is in the centre and contains the content.
+ 
 */
 
 'use strict';
