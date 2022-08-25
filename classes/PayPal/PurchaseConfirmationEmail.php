88cd6c62ca7fc5f6878b1be1ca60add7ab445eda
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-25 23:39:18 +0100

    Move PurchaseConfirmationEmail.php class to a separate file

---
+++ classes/PayPal/PurchaseConfirmationEmail.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Rosie\PayPal;
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use Rosie\PayPal\PayPalSDK\CaptureOrder;
+use Rosie\Utils\EnvironmentVariables;
+use Rosie\Utils\Logging;
+
+class PurchaseConfirmationEmail
+{
+    public function __construct(
+        public CaptureOrder $captureOrder,
+        public OrderInsertionIntoDatabase $orderInsertionIntoDatabase,
+        private Logging $logging
+    ) {
+    }
+
+    public function notifyCustomer(): void
+    {
+        EnvironmentVariables::getEnvVars();
+
+        $payPalEnvironment = EnvironmentVariables::$payPalEnvironment;
+        $sellerEmail = EnvironmentVariables::$sellerEmail;
+        $sellerPhone = EnvironmentVariables::$sellerPhone;
+        $sellerMobile = EnvironmentVariables::$sellerMobile;
+
+        $itemsDetails = $items = $products = $subtotal = $qtyArray = [];
+        $amount = $totalQty = 0;
+
+        if (!empty($_SESSION['itemsDetails'])) {
+            foreach ($_SESSION['itemsDetails'] as $productDetail) {
+                $product['name'] = $productDetail['name'];
+                $product['qty'] = $productDetail['quantity'];
+                $product['value'] = $productDetail['unit_amount']['value'];
+                $products[] = $product;
+            }
+            // Variable used below in purchase-confirmation-email.php
+            $items[] = $products;
+        }
+        $mail = new PHPMailer(true);
+        if ($payPalEnvironment === 'production') {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.hostinger.co.uk';
+            $mail->SMTPAuth = true;
+            $mail->Port = 587;
+            $mail->Username = EnvironmentVariables::$sellerEmailUsername;
+            $mail->Password = EnvironmentVariables::$sellerEmailPassword;
+        }
+
+        try {
+            $mail->setFrom($sellerEmail, 'Rosie Piontek Art');
+            if ($payPalEnvironment === 'production') {
+                $mail->addAddress($this->captureOrder->buyerEmail, $this->captureOrder->buyerName);
+            } elseif ($payPalEnvironment === 'sandbox') {
+                $mail->addAddress('testRosie@protonmail.com', $this->captureOrder->buyerName);
+            }
+            $mail->addReplyTo($sellerEmail, 'Rosie Piontek Art');
+            $mail->isHTML();
+            $mail->Subject = 'Purchase confirmation from Rosie Piontek Art. Order ID: ' . $this->captureOrder->orderId;
+            ob_start();
+            include '/home/andrzej/dev/rosie/views/purchase-confirmation-email.php';
+            $mail->Body = ob_get_clean();
+            $mail->AltBody = 'Confirmation is in plain text for non-HTML email readers.';
+            $mail->send();
+            // https://github.com/PHPMailer/PHPMailer/blob/master/examples/exceptions.phps
+        } catch (Exception $prettyPHPMailerErrorMessage) {
+//            $prettyPHPMailerErrorMessage->errorMessage()
+        } catch (\Exception $standardErrorMessage) {
+//            $standardErrorMessage->getMessage()
+        }
+    }
+}
