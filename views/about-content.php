63f88a2bb462d6420b15f8a7cf6869f6e52b6298
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-25 23:45:41 +0100

    Account for creating a dependency container class in about-content.php

--- views/about-content.php
+++ views/about-content.php
@@ -1,14 +1,14 @@
 <div class="about">
     <div class="about__img-wrapper">
-        <img class="about__img" src="img/img-about/rosie-about.jpg" alt="A photograph of Rosie Piontek" 
+        <img class="about__img" src="../img/img-about/rosie-about.jpg" alt="A photograph of Rosie Piontek"
              width="314" height="359">
     </div>
     <div class="about__txt-container">
 
-    <?php while ($aboutSectionContent = $resultAbout->fetch()): ?>
-
+    <?php
+    $result = $this->aboutDepCont->runDependencies();
+    while ($aboutSectionContent = $result->fetch()) : ?>
         <p class="about__txt"><?php echo $aboutSectionContent['paragraph']; ?></p>
-
     <?php endwhile; ?>
 
     </div>
