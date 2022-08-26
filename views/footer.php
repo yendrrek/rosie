20915e026017bafa605b7c1d6feffc9176590057
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-26 13:46:11 +0100

    Swap strpos() for str_contains()

--- views/footer.php
+++ views/footer.php
@@ -1,27 +1,28 @@
-<?php 
-if (strpos($pageTitle, 'Shop') !== false):
+<?php
+if (str_contains($pageTitle, 'Shop')) :
     include 'includes/postage-payments-returns-info-activator.html';
 endif;
-include 'includes/back-to-top-btn.html'; 
-    ?>
+include 'includes/back-to-top-btn.html';
+?>
     
-    <div class="body__push"></div> <!-- Keeps the footer always at the bottom. -->
-</div> <!-- Closing tag for all the content above the footer -->
-<footer class="footer body__footer">
+    <div class="push"></div> <!-- Keeps the footer always at the bottom. -->
+</div> <!-- Closing tag for all the content above the footer. The opening tag is in 'head.php'. -->
+<footer class="footer">
     <span class="copyright">&copy; <?php echo date('Y'); ?> Rosie Piontek</span>
     <a class="fab fa-instagram link link_visited link_instagram link_instagram_hover footer__link
     link_instagram_outline" href="https://www.instagram.com/rosiepiontekart/?hl=en" target="_blank"></a>
 </footer>
-<script src="scripts/explorer-alert.js"></script>
-<script type="module" src="scripts/main.js"></script>
+<script src="../scripts/explorer-alert.js"></script>
+<script type="module" src="../scripts/main.js"></script>
 
 <?php
-if (strpos($pageTitle, 'Basket') !== false):
-    if (!empty($_SESSION['basket'])):
-      include 'scripts/paypal-smart-btn-script.php';
+if (str_contains($pageTitle, 'Basket')) :
+    if (!empty($_SESSION['basket'])) :
+        include 'scripts/pay-pal/paypal-smart-btn.php';
     endif;
 endif
 ?>
 
+<!-- The opening tags are in 'head.php'. -->
 </body>
 </html>
