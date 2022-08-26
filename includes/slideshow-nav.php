5e941e4e5259ace899e88e6b2393556e12d21fd2
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-26 23:18:00 +0100

    Refactor code
    - Reuse separate static class for getting slideshow section title.
    
    - Reformat code.

--- includes/slideshow-nav.php
+++ includes/slideshow-nav.php
@@ -1,8 +1,10 @@
+<?php use Rosie\Utils\PageName; ?>
+
 <div class="slideshow__navigation">
-    <h2 class="slideshow__title"><?php echo $slideshowSectionTitle1; ?><!--
+    <h2 class="slideshow__title"><?php echo PageName::fitSlideshowSectionNameOnSmallScreen()[0]; ?><!--
      --><br class="slideshow__title_vertical">
         <span class="slideshow__title_2nd-word"><!--
-     --><?php if (isset($slideshowSectionTitle2)) echo $slideshowSectionTitle2; ?></span>
+     --><?php echo PageName::fitSlideshowSectionNameOnSmallScreen()[1] ?? ''; ?></span>
     </h2>
     <div class="slideshow__counter-and-arrows">
         <span class="icon slideshow__icon slideshow__icon_arrow-previous icon_hover material-icons">chevron_left
@@ -15,6 +17,8 @@
         <span class="icon slideshow__icon slideshow__icon_arrow-next icon_hover material-icons">chevron_right
         </span>
     </div>
-    <span class="icon icon_zoom slideshow__icon slideshow__icon_zoom icon_hover material-icons icon_zoom-and-close_outline">zoom_in</span>
-    <span class="icon icon_close slideshow__icon slideshow__icon_close icon_hover material-icons icon_zoom-and-close_outline">close</span>
+    <span class="icon icon_zoom slideshow__icon slideshow__icon_zoom icon_hover
+    material-icons icon_zoom-and-close_outline">zoom_in</span>
+    <span class="icon icon_close slideshow__icon slideshow__icon_close icon_hover
+    material-icons icon_zoom-and-close_outline">close</span>
 </div>
