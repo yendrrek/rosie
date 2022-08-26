9c80b84a8574c9b5e08e8883a9dbd2c2ec6ceb0f
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-26 13:55:29 +0100

    Refactor code
    - Change the extension of the file from .html to .php.
    - Insert css classes with a PHP class

---
+++ includes/logo.php
@@ -0,0 +1,12 @@
+<?php
+use Rosie\Utils\LogoColourNameOfCssClass;
+?>
+
+<a class="logo logo_hover logo_outline link link_background_transition link_visited" href="all-works.php">
+    <span class="logo__name">Rosie Pion<!--
+     --><span class="<?php echo LogoColourNameOfCssClass::setLogoColourCssClassName(); ?>">t</span>ek<!--
+    --></span>
+    <span class="logo__name-extension">- Traditional Artist<!--
+     --><span class="<?php echo LogoColourNameOfCssClass::setLogoColourCssClassName(); ?>">.</span>
+    </span>
+</a>
