994624ae9881c2b7ca33e448b973b09f9003b72e
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-27 00:12:01 +0100

    Get values directly from static variables

--- includes/phone-location-email-rosie.php
+++ includes/phone-location-email-rosie.php
@@ -1,11 +1,13 @@
-<div class="contact body__contact">
+<?php use Rosie\Utils\EnvironmentVariables; ?>
+
+<div class="contact contact">
     <div class="contact__container">
         <h3 class="contact__name">Rosie Piontek</h3>
         <p class="contact__details">
             Orkney, United Kingdom <br>
-            <?php echo $sellerPhone; ?><br>
-            <?php echo $sellerMobile; ?><br>
-            <?php echo $sellerEmail; ?>
+            <?php echo EnvironmentVariables::$sellerPhone;?><br>
+            <?php echo EnvironmentVariables::$sellerMobile;?><br>
+            <?php echo EnvironmentVariables::$sellerEmail;?>
         </p>
     </div>
 </div>
