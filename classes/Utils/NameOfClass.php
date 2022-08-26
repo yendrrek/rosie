1247e710b427c1001cb380f492dc754914a1c76d
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-26 23:12:46 +0100

    Dynamically create class names for controllers and services

---
+++ classes/Utils/NameOfClass.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Rosie\Utils;
+
+class NameOfClass
+{
+    public static function getClassName($instance): string
+    {
+        $pageName = PageName::getPageName();
+        $artworkPageNames = ['All Works', 'Geometry', 'Stained Glass', 'Ceramic Tiles', 'Paintings'];
+
+        if (in_array($pageName, $artworkPageNames)) {
+            return self::getArtworkClassName($instance);
+        }
+        return self::getProfaneClassName($pageName, $instance);
+    }
+
+    private static function getArtworkClassName($instance): string
+    {
+        return self::getLayer($instance, 'Artwork', 'ArtworkDepCont');
+    }
+
+    private static function getProfaneClassName($pageName, $instance): string
+    {
+        return self::getLayer($instance, self::removeSpace($pageName), self::removeSpace($pageName) . 'DepCont');
+    }
+
+    private static function getLayer($instance, $controller, $service): string
+    {
+        return $instance == 'controller' ? $controller : $service;
+    }
+
+    private static function removeSpace($className): string
+    {
+        return str_replace(' ', '', $className);
+    }
+}
