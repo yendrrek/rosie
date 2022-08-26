f29ff2b0cb5f76e8f0a1ebfcf4d99baa4ab69389
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-26 13:48:56 +0100

    Refactor code
    - Made code more readable by removing nested if/foreach statements/loops
    
    - Changed location of the class

---
+++ classes/Services/Basket/UpdatingProductQtyViaDropDownInBasket.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Rosie\Services\Basket;
+
+use Rosie\Utils\FormValidation;
+use Rosie\Utils\Logging;
+
+class UpdatingProductQtyViaDropDownInBasket
+{
+    public function __construct(
+        private Logging $logging,
+        private FormValidation $formValidation
+    ) {
+    }
+
+    public function updateProductQuantityViaBasketDropDownMenu(): bool
+    {
+        $invalidCsrfTokenMessage = 'Invalid or no token when updating product in basket';
+        $isNotProductQuantityBeingAmendedFromInsideBasket = !isset($_POST['quantityDropDownMenuIdInBasket']);
+
+        if (!$this->formValidation->validateForm(
+            'post',
+            $invalidCsrfTokenMessage,
+            $isNotProductQuantityBeingAmendedFromInsideBasket
+        )) {
+            return false;
+        }
+
+        foreach ($_SESSION['basket'] as &$product) {
+            if ($product['productId'] == $_POST['quantityDropDownMenuIdInBasket']) {
+                $product['productQuantity'] = $_POST['newQty'];
+
+                $this->logging->logMessage(
+                    'info',
+                    "Updating quantity of '{$product['productName']}' in basket to {$product['productQuantity']}"
+                );
+
+                if ($product['productQuantity'] == 0) {
+                    $this->removeProductWithZeroQuantityFromBasket(
+                        $product['productId']
+                    );
+                }
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function removeProductWithZeroQuantityFromBasket($productId): void
+    {
+        unset($_SESSION['basket'][$productId]);
+    }
+}
