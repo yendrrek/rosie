f43989d15340dc62c52e8542204ea4d97764b43e
Andrzej Piontek <yendrrek@protonmail.com>
2022-08-29 14:27:53 +0100

    Change variable name, add time zone
    - Messages are now logged using 'Europe/London', not 'UTC' (one hour difference).

--- classes/Utils/NewLogger.php
+++ classes/Utils/NewLogger.php
@@ -2,14 +2,15 @@
 
 namespace Rosie\Utils;
 
+use DateTimeZone;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 class NewLogger
 {
-    public function injectNewLogger($name): Logger
+    public function injectNewLogger($className): Logger
     {
-        $logger = new Logger($name);
+        $logger = new Logger($className, [], [], new DateTimeZone('Europe/London'));
         // Full path as otherwise PayPal files from directory 'scripts/paypal' can't write to 'rosie.log'.
         $logger->pushHandler(new StreamHandler('/home/andrzej/dev/rosie/log/rosie.log', Logger::INFO));
         return $logger;
