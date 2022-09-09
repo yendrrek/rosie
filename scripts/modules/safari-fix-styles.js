461adc9056a3b6cf5ecc6a5308edfbc6d7fc43e1
Andrzej Piontek <yendrrek@protonmail.com>
2022-09-09 14:36:33 +0100

    Refactor
    - Remove duplicate code by abstraction. Create one common function with arguments.

--- scripts/modules/safari-fix-styles.js
+++ scripts/modules/safari-fix-styles.js
@@ -1,29 +1,15 @@
 'use strict';
 
 export function fixStylesInSafariOnly () {
-  fixAddToBasketBtn();
-  fixExtraImgActivatorInShop();
-  disableChromeImgFix();
+  fix('.btn_add-to-basket', 'btn_add-to-basket_safari-font-fix');
+  fix('.extra-img-activator', 'word-more-pseudo_safari-position-fix');
+  fix('.slide__img-rectangle, .slide__img-square', 'slide__img_dont-apply-chrome-fix-in-safari');
 }
 
-function fixAddToBasketBtn () {
-  const addToBasketBtns = document.querySelectorAll('.btn_add-to-basket');
-  for (const btn of addToBasketBtns) {
-    btn.classList.add('btn_add-to-basket_safari-font-fix');
-  }
-}
-
-function fixExtraImgActivatorInShop () {
-  const extraImgActivators = document.querySelectorAll('.extra-img-activator');
-  for (const activator of extraImgActivators) {
-    activator.classList.add('word-more-pseudo_safari-position-fix');
-  }
-}
-
-function disableChromeImgFix () {
-  const slideshowImgs = document.querySelectorAll('.slide__img-rectangle, .slide__img-square');
-  for (const img of slideshowImgs) {
-    img.classList.add('slide__img_dont-apply-chrome-fix-in-safari');
+function fix(selector, fixingStyle) {
+  const elementsToFix = document.querySelectorAll(selector);
+  for (const element of elementsToFix) {
+    element.classList.add(fixingStyle);
   }
 }
   
