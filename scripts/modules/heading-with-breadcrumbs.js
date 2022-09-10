fe3f473d0188bf45a38ebbac11391e39e9177da6
Andrzej Piontek <yendrrek@protonmail.com>
2022-09-10 11:33:29 +0100

    Refactor
    - Remove duplicate code. Create one common function with arguments.
    - Remove 'else' and use 'return'.

--- scripts/modules/heading-with-breadcrumbs.js
+++ scripts/modules/heading-with-breadcrumbs.js
@@ -1,30 +1,28 @@
 'use strict';
 
-export function controlHeadingWithBreadcrumbs () {
+export function controlHeadingWithBreadcrumbs() {
+  const show = (element, style) => element.classList.add(style);
+  const hide = (element, style) => element.classList.remove(style);
+
+  if (document.documentElement.scrollTop > 118) {
+    toggleBreadcrumbsVisibility(show);
+    return;
+  }
+  toggleBreadcrumbsVisibility(hide);
+}
+
+function toggleBreadcrumbsVisibility(method) {
   const headingWithBreadCrumbs = document.querySelector('.breadcrumbs');
   const basketIcon = document.querySelector('.basket__icon_breadcrumbs_hidden');
   const wordBasket = document.querySelector('.basket__txt_hidden');
   const productQty = document.querySelector('.number_hidden');
-  const scrollDownPosition = document.documentElement.scrollTop;
-  if (headingWithBreadCrumbs) {
-    if (scrollDownPosition > 118) {
-      makeHeadingWithBreadcrumbsFixed(headingWithBreadCrumbs, basketIcon, wordBasket, productQty);
-    } else {
-      unfixHeadingWithBreadcrumbs(headingWithBreadCrumbs, basketIcon, wordBasket, productQty);
-    }
+
+  if (!headingWithBreadCrumbs) {
+    return;
   }
-}
 
-function makeHeadingWithBreadcrumbsFixed (headingWithBreadCrumbs, basketIcon, wordBasket, productQty) {
-  headingWithBreadCrumbs.classList.add('heading-with-breadcrumbs_fixed');
-  basketIcon.classList.add('heading-with-breadcrumbs__basket-icon_visible');
-  wordBasket.classList.add('heading-with-breadcrumbs__word-basket_visible');
-  productQty.classList.add('heading-with-breadcrumbs__product-qty_visible');
-}
-
-function unfixHeadingWithBreadcrumbs (headingWithBreadCrumbs, basketIcon, wordBasket, productQty) {
-  headingWithBreadCrumbs.classList.remove('heading-with-breadcrumbs_fixed');
-  basketIcon.classList.remove('heading-with-breadcrumbs__basket-icon_visible');
-  wordBasket.classList.remove('heading-with-breadcrumbs__word-basket_visible');
-  productQty.classList.remove('heading-with-breadcrumbs__product-qty_visible');
+  method(headingWithBreadCrumbs, 'heading-with-breadcrumbs_fixed');
+  method(basketIcon, 'heading-with-breadcrumbs__basket-icon_visible');
+  method(wordBasket, 'heading-with-breadcrumbs__word-basket_visible');
+  method(productQty, 'heading-with-breadcrumbs__product-qty_visible');
 }
