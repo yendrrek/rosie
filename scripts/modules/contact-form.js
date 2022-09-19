45b9c14649738e387ca16c33269e6b3f6055c185
Andrzej Piontek <yendrrek@protonmail.com>
2022-09-19 10:08:04 +0100

    Refactor
    - Remove duplicate code.
    - Wrap some functionalities in separate small functions.
    - Change names of functions and variables.

--- scripts/modules/contact-form.js
+++ scripts/modules/contact-form.js
@@ -2,109 +2,86 @@
 
 import { isInViewport } from './helper-methods.js';
 
-export function sendMessgeViaContactForm (event) {
+export function sendMessageViaContactForm (event) {
   event.preventDefault();
   $.ajax({
     method: 'POST',
     data: $(event.currentTarget).serialize(),
     dataType: 'text',
     success(response) {
-      showMsgSentOrFailedNotification(response);
-      makeNotificationVisibleIfNotInViewport();
-      if ($('#sender-name-error', response).text()) {
-        showNameError(response);
-      } else {
-        removeNameError(response);
-      }
-      if ($('#sender-email-error', response).text()) {
-        showEmailError(response);
-      } else {
-        removeEmailError(response);
-      }
-      if ($('#msg-error', response).text()) {
-        showMsgError(response);
-      } else {
-        removeMsgError(response);
-      }
+      toggleGeneralNotifications(response);
+      toggleFieldErrors(response);
+      showGeneralNotificationIfNotInViewport();
     }
   });
 }
 
-function showMsgSentOrFailedNotification (response) {
-  if ($('form').is(':first-child')) {
-    showNotificationFirstTime (response);
-  } else if ($('.contact-form__msg-error').is(':first-child')) {
-    showMsgSentIfFailedAlreadyDisplayed (response);
-  } else if ($('.contact-form__msg-success').is(':first-child')) {
-    showFailedIfMsgSentAlreadyDisplayed (response);
-  }
-}
+function toggleGeneralNotifications(response) {
+  const selectors = ['.contact-form__msg-error', '.contact-form__msg-success'];
 
-function showNotificationFirstTime (response) {
-  if ($('.contact-form__msg-error', response).text()) {
-    $('.contact-form').prepend($('.contact-form__msg-error', response));
-  } else if ($('.contact-form__msg-success', response).text()) {
-    $('.contact-form').prepend($('.contact-form__msg-success', response));
-  }
-}
+  $(selectors).each(index => {
+    if ($(selectors[index], response).text()) {
+      if (notificationWillBeShownFirstTime()) {
+        showNotificationFirstTime($(selectors[index], response));
+        return false;
+      }
 
-function showMsgSentIfFailedAlreadyDisplayed (response) {
-  if ($('.contact-form__msg-success', response).text()) {
-    $('.contact-form__msg-error').replaceWith($('.contact-form__msg-success', response));
-  }
-}
-
-function showFailedIfMsgSentAlreadyDisplayed (response) {
-  if ($('.contact-form__msg-error', response).text()) {
-    $('.contact-form__msg-success').replaceWith($('.contact-form__msg-error', response));
-  }
-}
-
-function makeNotificationVisibleIfNotInViewport () {
-  const contactFormNotifications = document.querySelectorAll('.contact-form__msg-success, .contact-form__msg-error');
-  for (const notification of contactFormNotifications) {
-    if (isInViewport(notification) === false) {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
+      replaceOppositeNotification($(selectors[index === 0 ? 1 : 0]), $(selectors[index], response));
     }
+  });
+}
+
+const notificationWillBeShownFirstTime = () => $('form').is(':first-child');
+const showNotificationFirstTime = selector => $('.contact-form').prepend(selector);
+const replaceOppositeNotification = (oppositeSelector, selector) => oppositeSelector.replaceWith(selector);
+
+function toggleFieldErrors(response) {
+  const selectors = ['#sender-name-error', '#sender-email-error', '#msg-error'];
+  const fields = [$('input[name="senderName"]'), $('input[name="senderEmail"]'), $('textarea[name="msg"]')];
+
+  $(selectors).each(index => {
+    if ($(selectors[index], response).text()) {
+      showFieldError(response, selectors[index], fields[index]);
+    } else {
+      hideFieldError(selectors[index], fields[index]);
+    }
+  });
+}
+
+function showFieldError(response, selector, field) {
+  $(selector).text($(selector, response).text());
+  showRedOutline(field);
+}
+
+function hideFieldError(selector, field) {
+  $(selector).text('');
+  hideRedOutline(field);
+}
+
+const showRedOutline = field => field.addClass('contact-form__error-outline_red');
+const hideRedOutline = field => field.removeClass('contact-form__error-outline_red');
+
+function showGeneralNotificationIfNotInViewport() {
+  const notifications = document.querySelectorAll('.contact-form__msg-success, .contact-form__msg-error');
+
+  for (const notification of notifications) {
+    if (isInViewport(notification)) {
+      return;
+    }
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
 }
 
-function showNameError (response) {
-  $('#sender-name-error').text($('#sender-name-error', response).text());
-  $("input[name='senderName']").addClass('contact-form__error-outline_red');
-}
+export function dontResubmitContactFormWhenPageReloaded() {
+  const contactFormPage = document.querySelector('input[name="name"]');
 
-function removeNameError () {
-  $('#sender-name-error').text('');
-  $("input[name='senderName']").removeClass('contact-form__error-outline_red');
-}
-
-function showEmailError (response) {
-  $('#sender-email-error').text($('#sender-email-error', response).text());
-  $("input[name='senderEmail']").addClass('contact-form__error-outline_red');
-}
-
-function removeEmailError () {
-  $('#sender-email-error').text('');
-  $("input[name='senderEmail']").removeClass('contact-form__error-outline_red');
-}
-
-function showMsgError (response) {
-  $('#msg-error').text($('#msg-error', response).text());
-  $("textarea[name='msg']").addClass('contact-form__error-outline_red');
-}
-
-function removeMsgError () {
-  $('#msg-error').text('');
-  $("textarea[name='msg']").removeClass('contact-form__error-outline_red');
-}
-
-export function dontResubmitContactFormWhenPageReloaded () {
-  const contactFormPage = document.querySelector("input[name='name']");
-  if (contactFormPage) {
-    window.history.replaceState(null, null, window.location.href);
+  if (!contactFormPage) {
+    return;
   }
+
+  window.history.replaceState(null, null, window.location.href);
 }
