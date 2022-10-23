8f74d0cc9302bed4ccf9f314f091b6bf48a53eae
Andrzej Piontek <yendrrek@protonmail.com>
2022-10-23 16:21:13 +0100

    Refactor
    - Use forEach() instead of 'for' loop in safari-fix-styles.js

--- scripts/modules/safari-fix-styles.js
+++ scripts/modules/safari-fix-styles.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export function fixStylesInSafariOnly() {
+export function fixElementsInSafariOnly() {
   fix('.btn_add-to-basket', 'btn_add-to-basket_safari-font-fix');
   fix('.extra-img-activator', 'word-more-pseudo_safari-position-fix');
   fix('.slide__img-rectangle, .slide__img-square', 'slide__img_dont-apply-chrome-fix-in-safari');
@@ -8,8 +8,6 @@ export function fixStylesInSafariOnly() {
 
 function fix(selector, fixingStyle) {
   const elementsToFix = document.querySelectorAll(selector);
-  for (const element of elementsToFix) {
-    element.classList.add(fixingStyle);
-  }
+  elementsToFix.forEach(element => element.classList.add(fixingStyle));
 }
   
