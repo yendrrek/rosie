53615de88e37422a046ee32f0acb39e05c27cf33
Andrzej Piontek <yendrrek@protonmail.com>
2022-10-29 17:39:40 +0100

    Create environment variable for the custom log file path
    - Account for different location of the file in prod and dev environments.

--- classes/Utils/NewLogger.php
+++ classes/Utils/NewLogger.php
@@ -11,8 +11,7 @@ class NewLogger
     public function injectNewLogger($className): Logger
     {
         $logger = new Logger($className, [], [], new DateTimeZone('Europe/London'));
-        // Full path as otherwise PayPal files from directory 'scripts/paypal' can't write to 'rosie.log'.
-        $logger->pushHandler(new StreamHandler('/home/andrzej/dev/rosie/log/rosie.log', Logger::INFO));
+        $logger->pushHandler(new StreamHandler(EnvironmentVariables::$logFilePath, Logger::INFO));
         return $logger;
     }
 }
